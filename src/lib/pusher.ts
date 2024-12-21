f3c2a6447cca00864f35d8d2253429de76693e99
amichaeel <mail@anthonymham.com>
2024-12-21 17:49:45 -0500

    update pusher cluster object;

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -2,10 +2,11 @@ import PusherServer from "pusher";
 import Pusher from "pusher-js"
 
 export const pusherServer = new PusherServer({
-  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
+  appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_PUBLISHABLE_KEY!,
   secret: process.env.PUSHER_SECRET_KEY!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  useTLS: true
 })
 
 export const pusherClient = new Pusher(
